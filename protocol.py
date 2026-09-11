bd11abaef56307ab3d8f96a4674b7bb2246dc5dd
Soong En Wong <wsoongen2015@gmail.com>
2026-09-11 13:42:45 +0100

    5 fields of 8 bytes each, so 40 bytes per tick

--- protocol.py
+++ protocol.py
@@ -10,3 +10,14 @@ TICK_DTYPE = np.dtype([
     ('bid_size',     np.float64), # 8 bytes: Volume at best bid
     ('ask_size',     np.float64), # 8 bytes: Volume at best ask
 ], align=True)
+
+TICK_ITEMSIZE = TICK_DTYPE.itemsize
+EXPECTED_OFFSETS = {
+    'timestamp_ns': 0,
+    'bid_price'   : 8,
+    'ask_price'   : 16,
+    'bid_size'    : 24,
+    'ask_size'    : 32
+}
+
+assert TICK_ITEMSIZE == 40, f"Protocol integrity broken: itemsize is {TICK_ITEMSIZE}, expected 40"
